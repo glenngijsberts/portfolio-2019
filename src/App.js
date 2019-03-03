c6f6a75f04d48b4843413e7ebf1bb678c8fc230d
Glenn Gijsberts <glenngijsberts95@gmail.com>
2019-03-03 18:47:47 +0100

    Init project with CRA

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+		<p>Hello World <span role="img" aria-label="fiyah">🔥</span></p>
+    );
+  }
+}
+
+export default App;
