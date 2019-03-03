bf9efc7190bcc981b6ac17969aa35216d1215e7b
Glenn Gijsberts <glenngijsberts95@gmail.com>
2019-03-03 22:37:25 +0100

    Prettier, eslint and editor config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
-    return (
-		<p>Hello World <span role="img" aria-label="fiyah">🔥</span></p>
-    );
-  }
+    render() {
+        return (
+            <p>
+                Hello World{' '}
+                <span role="img" aria-label="fiyah">
+                    🔥
+                </span>
+            </p>
+        )
+    }
 }
 
-export default App;
+export default App
