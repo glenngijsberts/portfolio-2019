65ee9c8d74d650ff8a2ab7104d0fbd49bf65cd3c
Glenn Gijsberts <glenngijsberts95@gmail.com>
2019-03-04 11:54:39 +0100

    Created a theme file

---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+export const color = {
+    black: '#16141f',
+    blackLighter: '#333040',
+    green: '#52dca3',
+    red: '#dc5258',
+    pink: '#e91e63',
+    pinkDarker: '#bf285b',
+}
+
+export const colorPrimary = color.pink
+export const colorPrimaryDarker = color.pinkDarker
+export const borderRadius = '4px'
